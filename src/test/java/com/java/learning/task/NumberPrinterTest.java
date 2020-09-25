835550dc88020c3311b7fe45c3324552e2739d0d
Sonali <sonali2708cheers@gmail.com>
2020-09-25 15:47:40 +0530

    code refactor and getting input max number from properties file

--- src/test/java/com/java/learning/task/NumberPrinterTest.java
+++ src/test/java/com/java/learning/task/NumberPrinterTest.java
@@ -5,13 +5,16 @@ import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 import com.java.learning.task.exception.LearningTaskException;
+import com.java.learning.task.util.PropertyUtil;
 
 /**
  * Test class tests various scenarios of print method printing 1 to 10
@@ -22,7 +25,8 @@ import com.java.learning.task.exception.LearningTaskException;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NumberPrinterTest {
 
-	private static final String INPUT_NUMBER = "10";
+	private static final String FOURTEEN = "14";
+	private static final String NUMBER = "Number :";
 	private NumberPrinter numberPrinter = new NumberPrinter();
 	private final PrintStream standardOut = System.out;
 	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
@@ -34,15 +38,17 @@ public class NumberPrinterTest {
 
 	@Test
 	public void testPrintNumbers_prints_1to10_successfully() throws LearningTaskException, IOException {
-		numberPrinter.printNumbers(INPUT_NUMBER);
+		Properties props = PropertyUtil.getInstance().getProperties(PropertyUtil.CONFIG_PROPERTIES);
+		PropertyUtil.getInstance().getPropertyValue(props, PropertyUtil.MAX_INPUT_NUMBER);
+		numberPrinter.printNumbers(PropertyUtil.getInstance().getPropertyValue(props, PropertyUtil.MAX_INPUT_NUMBER));
 		for (int i = 1; i <= 10; i++) {
-			assertEquals(true, outputStreamCaptor.toString().trim().contains("Number :" + i));
+			assertEquals(true, outputStreamCaptor.toString().trim().contains(NUMBER + i));
 		}
 	}
 
 	@Test(expected = Exception.class)
 	public void testPrintNumbers_when_max_input_number_is_not_10() throws LearningTaskException {
-		numberPrinter.printNumbers("14");
+		numberPrinter.printNumbers(FOURTEEN);
 	}
 
 	@Test(expected = Exception.class)
@@ -54,7 +60,7 @@ public class NumberPrinterTest {
 	@Test(expected = Exception.class)
 	public void testPrintNumbersFails_when_input_number_provided_is_empty() throws LearningTaskException {
 
-		numberPrinter.printNumbers("");
+		numberPrinter.printNumbers(StringUtils.EMPTY);
 	}
 
 	@After
--- src/test/java/com/java/learning/task/util/PropertyUtilTest.java
+++ src/test/java/com/java/learning/task/util/PropertyUtilTest.java
@@ -1,6 +1,7 @@
 package com.java.learning.task.util;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -8,16 +9,27 @@ import java.util.Properties;
 import org.junit.Test;
 
 public class PropertyUtilTest {
+	private static final String ANY_WRONG_KEY = "any wrong key";
+
 	@Test
-	public void testGetProperties() throws IOException {
+	public void testGetProperties_successful() throws IOException {
 		Properties props = PropertyUtil.getInstance().getProperties(PropertyUtil.CONFIG_PROPERTIES);
 		assertNotNull(props);
 	}
 	
 	@Test
-	public void testGetPropertyValue() throws IOException {
+	public void testGetPropertyValue_successful() throws IOException {
 		Properties props = PropertyUtil.getInstance().getProperties(PropertyUtil.CONFIG_PROPERTIES);
 		assertNotNull(props);
-		PropertyUtil.getInstance().getPropertyValue(props,PropertyUtil.MAX_INPUT_NUMBER);
+		String propertyValue = PropertyUtil.getInstance().getPropertyValue(props,PropertyUtil.MAX_INPUT_NUMBER);
+		assertNotNull(propertyValue);
+	}
+	
+	@Test
+	public void testGetPropertyValue_returns_null_value_when_provided_wrong_key() throws IOException {
+		Properties props = PropertyUtil.getInstance().getProperties(PropertyUtil.CONFIG_PROPERTIES);
+		assertNotNull(props);
+		String propertyValue = PropertyUtil.getInstance().getPropertyValue(props,ANY_WRONG_KEY);
+		assertNull(propertyValue);
 	}
 }
